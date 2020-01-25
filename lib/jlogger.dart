00e4c51ce56d8285f20f3b42f8ab9d25def981b0
Jhool <jhoolCode@gmail.com>
2020-01-25 16:39:45 +0530

    finishing touch

--- lib/jlogger.dart
+++ lib/jlogger.dart
@@ -1,16 +1,17 @@
+
+
 import 'dart:math';
 
 class JLogger
 {
 	static final stackTraceRegex = RegExp(r'#[0-9]+[\s]+(.+) \(([^\s]+)\)');
 	static final JLogger I = JLogger._internal();
-
-	factory JLogger() => I;
+	factory JLogger() => I ;
 
 	int debugMethodCount = 1;
 	int errorMethodCount = 8;
 
-	CLogPrinter logPrinter;
+	CLogPrinter logPrinter ;
 
 	JLogger._internal()
 	{
@@ -18,73 +19,74 @@ class JLogger
 	}
 
 
-	static void log(String msg, { StackTrace stackTrace, String label: '', int depth: 1})
+	static void log( String msg, { StackTrace stackTrace, String label: '', int depth : 1} )
 	{
-		I._show(msg, stackTrace: stackTrace, label: label, depth: depth);
+		I.show(msg, stackTrace: stackTrace, label: label, depth:depth);
 	}
 
 
-	void _show(String msg, { StackTrace stackTrace, String label: '', depth: 1})
+	void show( String msg, { StackTrace stackTrace, String label: '', depth:1} )
 	{
-		logPrinter._print(createJLog(msg, trace: stackTrace, label: label, depth: depth));
+		logPrinter._print( createJLog(msg, trace: stackTrace, label: label, depth: depth)  );
 	}
 
-	static void S(String msg, { StackTrace stackTrace, String label: '', int depth: 1})
+	static void S( String msg, { StackTrace stackTrace, String label: '', int depth : 1} )
 	{
-		I._show(msg, stackTrace: stackTrace, label: label, depth: depth);
+		I.show(msg, stackTrace: stackTrace, label: label, depth:depth);
 	}
 
 
-	JLog createJLog(String msg, { StackTrace trace, String label: '', depth: 1})
+
+	JLog createJLog( String msg, { StackTrace trace, String label: '', depth:1} )
 	{
-		JLog jLog = JLog(msg, depth, label: label);
+		JLog  jLog = JLog( msg, depth, label: label );
 
 		trace ??= StackTrace.current;
 
 		List<String> lines = trace.toString().split("\n");
 
+
 		int count = 0;
 
-		for (String line in lines)
+		for ( String line in lines)
 		{
 			var match = JLogger.stackTraceRegex.matchAsPrefix(line);
 			if (match != null)
 			{
-				if (match.group(2).startsWith('package:chat/jhul/logger'))
+				if (match.group(2).startsWith('package:jlogger/jlogger'))
 				{
 					continue;
 				}
 
-				String countPrefix = count
-					.toString()
-					.length < 2 ? '0' : '';
+				String countPrefix = count.toString().length < 2 ? '0' : '';
 
-				String newLine = '$countPrefix$count ${match.group(2)} :: ${match.group(1).split('.')[1]}()';
-				jLog.addLine(newLine.replaceAll('<anonymous closure>', '()'));
+				String newLine = '$countPrefix$count ${match.group(2)} :: ${match.group(1).split('.')[1]}()' ;
+				jLog.addLine( newLine.replaceAll('<anonymous closure>', '()') );
 
-				if (++count == jLog.traceDepth)
+				if ( ++count == jLog.traceDepth )
 				{
 					break;
 				}
 			}
 			else
 			{
-				jLog.addLine(line);
+				jLog.addLine( line );
 			}
 		}
 
 		return jLog;
 	}
+
 }
 
 class JLog
 {
 	final int traceDepth;
-	int width = 0;
+	int width = 0 ;
 
 	List<String> trace = [];
 
-	JLog(String msg, this.traceDepth, { String label = '' })
+	JLog(String msg, this.traceDepth, { String label = '' } )
 	{
 		if (label.length > 0)
 		{
@@ -94,12 +96,13 @@ class JLog
 		addLine('');
 		addLine(msg);
 		addLine('');
+
 	}
 
 
 	void addLine(String line)
 	{
-		if (line.length > width)
+		if( line.length > width )
 		{
 			width = line.length;
 		}
@@ -107,6 +110,7 @@ class JLog
 		trace.add(line);
 	}
 }
+
 
 
 class CLogPrinter
@@ -124,14 +128,15 @@ class CLogPrinter
 
 	final JLogger logger;
 
-	final int lineMaxLength = 120 - 4; // - |  |
+	final int lineMaxLength = 120 - 4 ; // - |  |
 
 	CLogPrinter(this.logger);
 
 	BoxClass box = Box();
 
-	void _print(JLog jLog)
+	void _print(  JLog jLog )
 	{
+
 		jLog.width = min(jLog.width, lineMaxLength);
 
 		_printBorderT(jLog);
@@ -142,40 +147,42 @@ class CLogPrinter
 	}
 
 
-	void _printBorderT(JLog jLog)
+
+	void _printBorderT( JLog jLog )
 	{
-		print(box.cornerTL + _createBorderH(jLog.width + 2) + box.cornerTR);
+		print( box.cornerTL + _createBorderH( jLog.width + 2 ) + box.cornerTR );
 	}
 
-	void _printBorderB(JLog jLog)
+	void _printBorderB( JLog jLog )
 	{
-		print(box.cornerBL + _createBorderH(jLog.width + 2) + box.cornerBR);
+		print( box.cornerBL + _createBorderH( jLog.width + 2 ) + box.cornerBR );
 	}
 
 
-	String _createBorderH(int msgLength)
+	String _createBorderH( int msgLength )
 	{
 		String line = '';
 
-		for (int i = msgLength; i >= 1; i--)
+		for( int i = msgLength ; i >= 1; i-- )
 		{
-			line = line + hChar;
+			line = line + hChar ;
 		}
 
-		return line;
+		return line ;
 	}
 
-	void printLines(JLog jStackTrace)
+	void printLines( JLog jStackTrace )
 	{
-		for (String trace in jStackTrace.trace)
+		for( String trace in jStackTrace.trace )
 		{
 			if (trace.length > lineMaxLength)
 			{
+
 				List<String> lines = splitByLength(trace, lineMaxLength);
 
-				for (String line in lines)
+				for( String line in lines )
 				{
-					_printLine(line, jStackTrace.width);
+					_printLine( line, jStackTrace.width);
 				}
 			}
 			else
@@ -185,17 +192,17 @@ class CLogPrinter
 		}
 	}
 
-	void _printLine(String trace, int width)
+	void _printLine( String trace, int width )
 	{
-		print(_createLine(trace, width));
+		print( _createLine( trace, width ) );
 	}
 
 
-	String _createLine(String msg, int boxWidth)
+	String _createLine( String msg, int boxWidth )
 	{
-		int appendWidth = boxWidth - msg.length + 1;
+		int appendWidth = boxWidth - msg.length + 1 ;
 
-		return vChar + ' ' + msg + (' ' * appendWidth) + vChar;
+		return vChar + ' ' + msg + (' ' * appendWidth)  + vChar;
 	}
 
 	List<String> splitByLength(String srcString, int size)
@@ -209,6 +216,7 @@ class CLogPrinter
 		}
 
 		return parts;
+
 	}
 
 }
@@ -216,19 +224,16 @@ class CLogPrinter
 
 abstract class BoxClass
 {
-	String get cornerTL;
+	String get cornerTL ;
 
 	String get cornerTR;
 
-	String get cornerBL;
+	String get  cornerBL;
+	String get  cornerBR;
 
-	String get cornerBR;
-
-	String get middleCorner;
-
-	String get vChar;
-
-	String get hChar;
+	String get  middleCorner;
+	String get  vChar;
+	String get  hChar;
 }
 
 class Box extends BoxClass
